213812073312c97f74004e26c9826fb3f86df358
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-02-24 14:28:46 +0000

    Not enought commits

--- lib/review_3.rb
+++ lib/review_3.rb
@@ -1,3 +1,15 @@
 def spell_check(string)
-  string
+  string = string.downcase
+  dictionary = ["cat", "dog", "the", "nice"]
+  additions = "The quick brown fox jumps over the lazy dog".downcase.split(" ")
+  dictionary.concat(additions)
+  string_array = string.split(" ")
+  checked = string_array.map { |string|
+    if dictionary.include? string
+      string
+    else
+      "~" + string + "~"
+    end
+    }
+  checked.join(" ")
 end
--- spec/review_3_spec.rb
+++ spec/review_3_spec.rb
@@ -14,5 +14,47 @@ RSpec.describe "Spell Checker" do
     expect(spell_check("cat")).to eq "cat"
   end
 
+  it "Checks single word spelled incorrectly" do
+    expect(spell_check("ccc")).to eq "~ccc~"
+  end
 
+  it "Checks single word spelled incorrectly" do
+    expect(spell_check("ddd")).to eq "~ddd~"
+  end
+
+  it "Checks two words" do
+    expect(spell_check("cat dog")).to eq "cat dog"
+  end
+
+  it "Checks two words" do
+    expect(spell_check("the nice")).to eq "the nice"
+  end
+
+  it "Checks two incorrectly spelt words" do
+    expect(spell_check("ttt nnnn")).to eq "~ttt~ ~nnnn~"
+  end
+
+  it "Checks case sensitivity" do
+    expect(spell_check("cAT")).to eq "cat"
+  end
+
+  it "Checks case sensitivity" do
+    expect(spell_check("DOG")).to eq "dog"
+  end
+
+  it "Checks single word spelled incorrectly, case sensitivity" do
+    expect(spell_check("ddD")).to eq "~ddd~"
+  end
+
+  it "Checks single word spelled incorrectly, case sensitivity" do
+    expect(spell_check("dSS")).to eq "~dss~"
+  end
+
+  it "Checks a sentence" do
+    expect(spell_check("The quick brown fox jumps over the lazy dog")).to eq "The quick brown fox jumps over the lazy dog".downcase
+  end
+
+  it "Checks a sentence" do
+    expect(spell_check("The quick bbbbb fox jumps over the lazy dog")).to eq "The quick ~bbbbb~ fox jumps over the lazy dog".downcase
+  end
 end
