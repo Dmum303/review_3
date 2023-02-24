4d62ba73f805f8963277535aacb9ea06cebc5eb2
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-02-24 14:11:40 +0000

    Single word check tests passed

--- lib/review_3.rb
+++ lib/review_3.rb
@@ -1,3 +1,3 @@
 def spell_check(string)
-
+  string
 end
--- spec/review_3_spec.rb
+++ spec/review_3_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe "Spell Checker" do
   it "tests the truth" do
     expect(true).to eq true
   end
+
+  it "Checks single word" do
+    expect(spell_check("dog")).to eq "dog"
+  end
+
+  it "Checks single word" do
+    expect(spell_check("cat")).to eq "cat"
+  end
+
+
 end
